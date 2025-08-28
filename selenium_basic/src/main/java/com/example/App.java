1a0afb77f48d59dd59aff9e7c1475abf6535b02c
himanshu singh <hchaudhary1792000@gmail.com>
2025-08-29 00:08:38 +0530

    new code

--- selenium_basic/src/main/java/com/example/App.java
+++ selenium_basic/src/main/java/com/example/App.java
@@ -1,6 +1,8 @@
 package com.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
@@ -11,8 +13,26 @@ public class App
 {
     public static void main( String[] args ) throws InterruptedException{
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        Thread.sleep(5000);
+        driver.get("https://www.facebook.com/");
+
+        WebElement element = driver.findElement(By.name("email"));
+        element.sendKeys("Himanshu");
+        // WebElement element2 =  driver.findElement(By.id("pass"));
+        WebElement element2 = driver.findElement(By.xpath("//input[@type='password']"));
+        element2.sendKeys("lunda le mera");
+
+        WebElement imgelement =  driver.findElement(By.xpath("//h2[@class='_8eso']"));
+        System.out.println( imgelement.getText());
+
+        // WebElement btn = driver.findElement(By.xpath("//*[@class='_42ft _4jy0 _6lth _4jy6 _4jy1 selected _51sy']"));
+        // WebElement btn = driver.findElement(By.xpath("//*[@class='_42ft _4jy0 _6lth _4jy6 _4jy1 selected _51sy']"));
+        // WebElement btn = driver.findElement(By.cssSelector("._42ft._4jy0._6lth._4jy6._4jy1.selected._51sy"));  
+         driver.findElement(By.cssSelector("button[name='login']")).click();
+        
+        // btn.click();
+        Thread.sleep(4000);
+        // driver.quit();
+        
     }
       
 }
